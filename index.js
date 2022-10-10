6f0cea54356aaa9ea85b80c5e473261394662d66
mramazzini <mramazzini123@gmail.com>
2022-10-09 20:43:09 -0500

    addRole() added

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function updateEmployeeRole(){
   inquirer.prompt([
     {
       type: 'input',
-      message: 'What is the id of the Employee\n',
+      message: 'What is the ID of the Employee\n',
       name: 'id'
     },
     {
@@ -124,6 +124,11 @@ function updateEmployeeRole(){
     }
   ])
   .then((res) => {
+    if(!isNumeric(res.id)){
+      console.log('Failed to update employee - ID must be a number');
+      openMenu();
+      return;
+  }
     let role = res.role;
     db.query('SELECT id, title FROM role', function (err, results) {
       for (let i=0; i<results.length; i++){
@@ -132,7 +137,6 @@ function updateEmployeeRole(){
           db.query('UPDATE employee SET role_id = "'+roleID+'" WHERE id = '+res.id, function (err, results) {
             console.log('\n');
             console.log('Successfully updated employee role');
-            viewEmployees();
           });
           openMenu();
           return;
@@ -151,20 +155,57 @@ function viewAllRoles(){
   openMenu();
 }
 
+//Checks if string is numeric
+function isNumeric(str) {
+  if (typeof str != "string") return false 
+  return !isNaN(str) && 
+         !isNaN(parseFloat(str)) 
+}
+
 function addRole(){
   inquirer.prompt([
     {
       type: 'input',
       message: 'What is the name of the new role\n',
-      name: 'id'
+      name: 'role'
+    },
+    {
+      type: 'input',
+      message: 'What is the salary of the role\n',
+      name: 'salary'
     },
     {
       type: 'input',
       message: 'What department is it under\n',
-      name: 'role'
+      name: 'department'
     }
   ])
   .then((res) => {
+    db.query('SELECT id, name FROM department', function (err, results) {
+      if(!isNumeric(res.salary)){
+          console.log('Failed to add Role - Salary must be a number');
+          openMenu();
+          return;
+      }
+      for (let i=0; i<results.length; i++){
+
+        if(results[i].name == res.department){
+          let departmentID = results[i].id;
+          db.query('SELECT COUNT(id) FROM role', function (err, results) {
+            let newID = Object.values(results[0])[0]+1;
+            db.query('INSERT INTO role (id, department_id, title, salary) VALUES ('+newID+', '+departmentID+', "'+res.role+'", '+res.salary+')', function (err, results) {
+              console.log('Successfully added role');
+            });
+          });
+          openMenu();
+          return;
+        }
+      }
+
+      console.log('Failed to add role - Department does not exist');
+        openMenu();
+  });
+})
 }
 
 function viewAllDepartments(){
